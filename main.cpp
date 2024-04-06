5c312e85159e1e3193bcb1a6effe1e40c939d0f8
Abdallah-El-Sakka <abdallahessam501@gmail.com>
2024-04-06 02:30:58 +0200

    Files Fixes

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"Advanced_DS/Linked_List.cpp"
+
+using namespace std;
+
+int main()
+{
+    LinkedList list1 = LinkedList();
+
+    // list1.insertEnd(50);
+
+    // list1.printData();
+
+}
