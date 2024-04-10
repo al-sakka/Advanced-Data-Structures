3ef96341dade89ca522a8098dc19d341c39c152c
Abdallah-El-Sakka <abdallahessam501@gmail.com>
2024-04-10 20:26:30 +0200

    more bug fixes

--- functions.cpp
+++ functions.cpp
@@ -100,7 +100,7 @@ void _stack()
     {
         cout << "Choose an operation to do : \n";
         cout << " 1) push \n 2) pop \n 3) peek \n 4) printAll \n 5) size \n 6) clear \n 7) swap \n 8) shuffle \n 9) back to menu \n";
-        
+
         int16_t nOfChoices = 9;
         int16_t choice;
 
@@ -167,7 +167,7 @@ void _queue()
     {
         cout << "Choose an operation to do : \n";
         cout << " 1) push \n 2) pop \n 3) peek \n 4) printData \n 5) size \n 6) clear \n 7) back to menu \n";
-        
+
         int16_t nOfChoices = 7;
         int16_t choice;
 
@@ -211,4 +211,38 @@ void _queue()
             cout << "Enter number between 1 and " << nOfChoices << endl;
         }
     }
+}
+
+
+int performUI()
+{
+    while (1)
+    {
+        cout << "Choose a data structure : " << endl;
+        cout << " 1) Linked List \n 2) Stack \n 3) Queue \n 4) Exit \n ";
+        int16_t nOfChoices = 4;
+
+        int16_t choice;
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            _linkedList();
+            break;
+        case 2:
+            _stack();
+            break;
+        case 3:
+            _queue();
+            break;
+        case 4:
+            return 0;
+        default:
+            cout << "Choose between 1 and " << nOfChoices << endl;
+            break;
+        }
+    }
+
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,35 +1,15 @@
-#include <iostream>
+/*
+    An application to learn more about data structres and switch easily between them.
+    Created by : Abdallah El-Sakka
+    Date : 10/4/2024
+*/
+
 #include "functions.cpp"
-using namespace std;
 
 int main()
 {
-
-    while (1)
-    {
-        cout << "Choose a data structure : " << endl;
-        cout << " 1) Linked List \n 2) Stack \n 3) Queue \n 4) Exit \n ";
-        int16_t nOfChoices = 4;
-
-        int16_t choice;
-        cin >> choice;
-
-        switch (choice)
-        {
-        case 1:
-            _linkedList();
-            break;
-        case 2:
-            _stack();
-            break;
-        case 3:
-            _queue();
-            break;
-        case 4:
-            return 0;
-        default:
-            cout << "Choose between 1 and " << nOfChoices << endl;
-            break;
-        }
-    }
+    /*
+        simple UI to choose between different data structures.
+    */
+    return performUI();
 }
